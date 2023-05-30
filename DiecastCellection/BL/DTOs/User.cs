225bfd69284a29b7d796203464cecac8e993513f
Saqib <20-43435-1@student.aiub.edu>
2023-05-30 23:55:31 +0600

    created till DBSET

---
+++ DiecastCellection/BL/DTOs/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.DTOs
+{
+    public class User
+    {
+        public int uid { get; set; }
+        public string uname { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public int totalCar { get; set; }
+
+        public virtual ICollection<Cars> Cars { get; set; }
+
+        public User()
+        {
+            this.Cars = new HashSet<Cars>(); // Hashset for omiting the duplicate 
+        }
+
+    }
+}
---
+++ DiecastCellection/DAL/Models/Cars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Cars
+    {
+        public int carId { get; set; }
+        public string modelName { get; set; }
+        public string brandName { get; set; }
+        public DateTime published { get; set; }
+        public int total { get; set; }
+        public string makeName { get; set; }
+        public string scale { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+        public Cars()
+        {
+            this.Users = new HashSet<User>(); // this hashset is to ommit the duplicate data 
+        }
+
+    }
+}
